4530f9e22fc0e7b72dddc1ff1728351db45b55c1
erik <erik@resourceatlanta.com>
2019-02-08 14:06:30 -0500

    integrate .bg-image CSS

--- usage.blade.php
+++ usage.blade.php
@@ -1,5 +1,5 @@
 // PHP / Blade fragment
 
-<div class="carousel-image {{ !empty(get_sub_field('image')['id']) ? 'srcset-background-image' : '' }}">
+<div class="carousel-image bg-image {{ !empty(get_sub_field('image')['id']) ? 'srcset-background-image' : '' }}">
     {!! wp_get_attachment_image( get_sub_field('image')['id'], 'full', false, array( 'class' => 'img-fluid d-none' ) ) !!}
 </div>
