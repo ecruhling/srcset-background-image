a57464074bbf2f375807062b7928f93f992bc46b
erik <erik@resourceatlanta.com>
2019-02-08 13:44:53 -0500

    fix terminology

--- usage.blade.php
+++ usage.blade.php
@@ -1,4 +1,4 @@
-// HTML fragment
+// PHP / Blade fragment
 
 <div class="carousel-image {{ !empty(get_sub_field('image')['id']) ? 'srcset-background-image' : '' }}">
     {!! wp_get_attachment_image( get_sub_field('image')['id'], 'full', false, array( 'class' => 'img-fluid d-none' ) ) !!}
